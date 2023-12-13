2441949211b83468110021fcfd56dc7cc203b0d5
Filip Pavlovič <xpavlov3@fi.muni.cz>
2023-12-13 19:17:46 +0100

    add two numbers faster iterative version

--- add_two_numbers/add_two_numbers.go
+++ add_two_numbers/add_two_numbers.go
@@ -10,34 +10,29 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 
     head := &ListNode{Val: 0, Next: nil}
     tail := head
+
     l, r := l1, l2
-    for l != nil && r != nil {
-        tail.Next = &ListNode{Val: (l.Val + r.Val + carry) % 10, Next: nil}
-        carry = (l.Val + r.Val + carry) / 10
+    lv, rv := 0, 0
+    for l != nil || r != nil || carry > 0 {
+        if (l != nil) {
+            lv = l.Val
+            l = l.Next
+        } else {
+            lv = 0
+        }
+
+        if (r != nil) {
+            rv = r.Val
+            r = r.Next
+        } else {
+            rv = 0
+        }
+
+        tail.Next = &ListNode{Val: (lv + rv + carry) % 10, Next: nil}
+        carry = (lv + rv + carry) / 10
         tail = tail.Next
-        l = l.Next
-        r = r.Next
     }
 
-    for l != nil {
-        tail.Next = &ListNode{Val: (l.Val + carry) % 10, Next: nil}
-        carry = (l.Val + carry) / 10
-        tail = tail.Next
-        l = l.Next
-    }
-
-    for r != nil {
-        tail.Next = &ListNode{Val: (r.Val + carry) % 10, Next: nil}
-        carry = (r.Val + carry) / 10
-        tail = tail.Next
-        r = r.Next
-    }
-
-    for carry > 0 {
-        tail.Next = &ListNode{Val: carry % 10, Next: nil}
-        carry /= 10
-        tail = tail.Next
-    }
 
     head = head.Next
     return head
